a3052a805e11a282b69c2369a3dc50eaf3e14d88
Gamal Osama <gamal8686@gmail.com>
2026-02-12 09:24:40 +0200

    app_Input

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../core/components/app_inpt.dart';
+import '../core/components/app_input.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
